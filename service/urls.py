24ea529eb7420c000e397a351db6f642fe9a110f
Denys Bokov <denisbokov1703@gmail.com>
2025-06-29 17:05:02 +0200

    feat(service): create UserCreateView using forms

--- service/urls.py
+++ service/urls.py
@@ -1,12 +1,13 @@
-
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
-from .views import index
+from .views import index, UserCreateView
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/registrate', UserCreateView.as_view(), name='registrate'),
 ] + debug_toolbar_urls()
 
 app_name = 'service'
--- service/views.py
+++ service/views.py
@@ -1,5 +1,30 @@
+from django.contrib.auth import get_user_model, login
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import UserCreateForm
+
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'pages/index.html')
+
+
+class UserCreateView(CreateView):
+    model = get_user_model()
+    form_class = UserCreateForm
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('service:index')
+
+    def get_success_url(self):
+        return reverse_lazy('service:index')
+
+    def form_valid(self, form):
+        self.object = form.save()
+
+        if not self.request.user.is_authenticated:
+            user = self.object
+            login(self.request, user)
+
+        return redirect(self.get_success_url())
