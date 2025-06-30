f2d98047c52340bcd82b1b3c541dc5d920573a6a
Denys Bokov <denisbokov1703@gmail.com>
2025-06-30 20:03:59 +0200

    feat(service): create Currency model and register it in admin

--- service/models.py
+++ service/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     pass
+
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    symbol = models.CharField(max_length=10)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'currency'
+        verbose_name_plural = 'currencies'
+
+    def __str__(self):
+        return f"{self.code} - {self.name} - {self.symbol}"
