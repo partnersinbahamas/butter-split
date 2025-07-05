2f980ab19c67e9fb654940d64f07a09edc9ace53
Denys Bokov <denisbokov1703@gmail.com>
2025-07-05 19:58:18 +0200

    feat(service): create Event and  Participant models

--- service/models.py
+++ service/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -17,4 +18,53 @@ class Currency(models.Model):
         verbose_name_plural = 'currencies'
 
     def __str__(self):
-        return f"{self.code} - {self.name} - {self.symbol}"
+        return f"{self.symbol} - {self.code}"
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='participants'
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Participant'
+        verbose_name_plural = 'Participants'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'creator'],
+                name='unique_creator_participant_name'
+            )
+        ]
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    participants = models.ManyToManyField(Participant, related_name='events')
+    currency = models.ForeignKey(
+        Currency,
+        on_delete=models.CASCADE,
+        related_name='events'
+    )
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='events'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+    def __str__(self):
+        return self.name
