ea9aa7a430fafbf7ec80192b3dc4ff3b9fa6588e
Denys Bokov <denisbokov1703@gmail.com>
2025-07-07 21:02:52 +0200

    feat(admin): register Event model in admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Currency, Participant
+from .models import Currency, Event, Participant
 
 
 @admin.register(Currency)
@@ -18,3 +18,17 @@ class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('name', )
     search_fields = ('name', )
     list_filter = ('name', )
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'currency', 'participants_count', 'created_at')
+    ordering = ('participants', 'created_at')
+    search_fields = ('name', )
+    list_filter = ('owner', 'currency')
+
+    def participants_count(self, obj):
+        return obj.participants.count()
+
+    participants_count.admin_order_field = 'participants'
+    participants_count.short_description = 'Number of participants'
