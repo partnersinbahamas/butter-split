5e66169834bdbe7f57e42ddf2ef69708f30cd833
Denys Bokov <denisbokov1703@gmail.com>
2025-07-29 21:24:13 +0200

    feat(templatetags): create query_transform simple tag

---
+++ service/templatetags/query_transform.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag()
+def query_transform(request, **kwargs):
+    query = request.GET.copy()
+
+    for key, value in kwargs.items():
+        if value is not None:
+            query[key] = value
+        else:
+            query.pop(key, 0)
+
+    return query.urlencode()
